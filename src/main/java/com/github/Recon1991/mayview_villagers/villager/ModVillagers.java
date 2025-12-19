5728d369f83191ab1b970c69bff789dfd7f7a2b2
recon <kalanicook@yahoo.com>
2025-12-19 04:27:20 -1000

    feature/created pokeballvendor_poi and villager in ModVillagers.java

--- src/main/java/com/github/Recon1991/mayview_villagers/villager/ModVillagers.java
+++ src/main/java/com/github/Recon1991/mayview_villagers/villager/ModVillagers.java
@@ -1,10 +1,17 @@
 package com.github.Recon1991.mayview_villagers.villager;
 
 import com.github.Recon1991.mayview_villagers.MayviewVillagers;
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.client.event.sound.SoundEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModVillagers {
@@ -12,6 +19,15 @@ public class ModVillagers {
             DeferredRegister.create(BuiltInRegistries.POINT_OF_INTEREST_TYPE, MayviewVillagers.MOD_ID);
     public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS =
             DeferredRegister.create(BuiltInRegistries.VILLAGER_PROFESSION, MayviewVillagers.MOD_ID);
+
+    public static final Holder<PoiType> POKEBALLVENDOR_POI = POI_TYPES.register("pokeballvendor_poi",
+            () -> new PoiType(ImmutableSet.copyOf(Blocks.EMERALD_BLOCK.getStateDefinition().getPossibleStates()), 1, 1));
+
+    public static final Holder<VillagerProfession> POKEBALLVENDOR = VILLAGER_PROFESSIONS.register("pokeballvendor",
+            () -> new VillagerProfession("pokeballvendor", holder -> holder.value() == POKEBALLVENDOR_POI.value(),
+                    poiTypeHolder -> poiTypeHolder.value() == POKEBALLVENDOR_POI.value(), ImmutableSet.of(), ImmutableSet.of(),
+                    SoundEvents.ENCHANTMENT_TABLE_USE
+            ));
 
     public static void register(IEventBus eventBus) {
         POI_TYPES.register(eventBus);
